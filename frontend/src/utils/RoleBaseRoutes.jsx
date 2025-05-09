126c4966a3ab0e6d2bd956301bfccc2d9281643a
Navikshaa <jineus@navikshaa.tech>
2025-05-09 23:26:10 +0530

    comment
    added hr and manager

--- frontend/src/utils/RoleBaseRoutes.jsx
+++ frontend/src/utils/RoleBaseRoutes.jsx
@@ -4,6 +4,7 @@ import { useAuth } from "../context/authContext";
 
 const RoleBaseRoutes = ({children, requiredRole}) => {
     const { user, loading } = useAuth()
+    const Navigate = useNavigate();
 
     if (loading) {
         return <div>Loading...</div>
